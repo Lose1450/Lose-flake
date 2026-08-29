b69106a9d0067698df3c4373fdc5a9f9327abab2
Lose <Losere1450@gmail.com>
2026-08-29 01:11:22 -0700

    Ver-1.4.6 added more drivers settings

--- configuration.nix
+++ configuration.nix
@@ -73,6 +73,8 @@
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
+    extraPackages = with pkgs; [ mesa ];
+    extraPackages32 = with pkgs.pkgsi686Linux; [ mesa ];
   };
 
   #bluetooth
