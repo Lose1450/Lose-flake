5b44ec711c41014749fa347a7d29c05ecf4849c3
Lose <Losere1450@gmail.com>
2026-08-28 18:02:02 -0700

    Ver-1.4.1 (8/28/26) added apps that Caelestia has dotfiles for

--- configuration.nix
+++ configuration.nix
@@ -125,7 +125,9 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     ghostty
-    vscodium
+    vscode
+    foot
+    thunar
     git
     tealdeer
     xclip
