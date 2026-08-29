6149afd673d1862452bb721b5145c484aba4bede
Lose <Losere1450@gmail.com>
2026-08-28 23:18:22 -0700

    Ver-1.4.2 minor change to have more efficient git and rebuild pipeline

--- home.nix
+++ home.nix
@@ -11,8 +11,8 @@
       btw = "echo i use nixos btw"; #this is just for demo purposes
       nrs = "sudo nixos-rebuild switch";
       nrsf = "sudo nixos-rebuild switch --flake /etc/nixos#Remielle";
-      rebuild-nixos = "sudo nixos-rebuild switch --flake /etc/nixos#Remielle && sudo cp /etc/nixos/* ~/Remielle && cd ~/Remielle && git add . && git commit && git push lose master";
-      git-commit = " sudo cp /etc/nixos/* ~/Remielle && cd ~/Remielle && git add . && git commit && git push lose master";
+      rebuild-nixos = "sudo nixos-rebuild switch --flake /etc/nixos#Remielle && sudo cp /etc/nixos/* ~/Remielle && cd ~/Remielle && git pull lose master && git status && git add . && git status && git commit && git push lose master";
+      git-commit = "sudo cp /etc/nixos/* ~/Remielle && cd ~/Remielle && git status && git add . && git status && git commit && git push lose master";
 
       };
 
