6a1534b6f056067b967b0b0661d79f9f536859f7
Lose <Losere1450@gmail.com>
2026-09-12 20:28:05 -0700

    Ver-1.5.8 removed ipod stuff

--- configuration.nix
+++ configuration.nix
@@ -178,8 +178,6 @@
     cliphist
     hyprpolkitagent
     inputs.caelestia-shell.packages.x86_64-linux.with-cli
-    rhythmbox
-    libgpod
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
